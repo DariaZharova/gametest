fdcaa6b1cbd44cff8e8e8d1ef640f4a36baf2ec6
DariaZharova <40871027+DariaZharova@users.noreply.github.com>
2026-02-13 01:25:31 +0300

    Delete bot directory

--- bot/utils/validators.py
+++
@@ -1,9 +0,0 @@
-import json
-from bot.models import RouteResult
-
-def validate_route_json(json_str: str) -> RouteResult | None:
-    try:
-        data = json.loads(json_str)
-        return RouteResult(**data)
-    except:
-        return None
